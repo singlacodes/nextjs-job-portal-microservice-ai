ce6240d7fbe3f43d02c17bdabc2075a2e6f84b30
singlacodes <shivamsinglatechnical@gmail.com>
2026-04-04 12:26:05 +0530

    feat(kafka): add Kafka producer for sending email notifications and update dependencies

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.js";
-const app = express();
+import { connectKafka } from "./producer.js";
+import cors from "cors";
 
+const app = express();
+app.use(cors());
 app.use(express.json());
+
+connectKafka();
+
 app.use("/api/auth", authRoutes);
+
 export default app;
